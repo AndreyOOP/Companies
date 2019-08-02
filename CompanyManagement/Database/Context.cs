6fb30248cdf981f7a5be8f0927f8780621bee642
AndreyOOP <andrew_yudaichev@i.ua>
2019-08-03 00:44:56 +0300

    Add constructor for connection string setting Add NUnit framework to project Add sample of test

--- CompanyManagement/Database/Context.cs
+++ CompanyManagement/Database/Context.cs
@@ -5,6 +5,10 @@ namespace CompanyManagement.Database
 {
     public class Context : DbContext
     {
+        public Context(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
+
         public DbSet<Company> Companies { get; set; }
     }
 }
